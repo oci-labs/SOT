5ae3a4167de84663607280d6ea5ed8a24e4540b9
ocimk <kafashanm@objectcomputing.com>
2019-04-02 15:43:00 -0500

    working end-end iot core script without gateway

--- cloud-iot-direct/security_cam_cloudiot.py
+++ cloud-iot-direct/security_cam_cloudiot.py
@@ -210,201 +210,14 @@ def listen_for_messages(
         gateway_id, num_messages, private_key_file, algorithm, ca_certs,
         mqtt_bridge_hostname, mqtt_bridge_port, jwt_expires_minutes, duration,
         cb=None):
-    """Listens for messages sent to the gateway and bound devices."""
-    # [START listen_for_messages]
-    global minimum_backoff_time
-
-    jwt_iat = datetime.datetime.utcnow()
-    jwt_exp_mins = jwt_expires_minutes
-    # Use gateway to connect to server
-    client = get_client(
-        project_id, cloud_region, registry_id, gateway_id,
-        private_key_file, algorithm, ca_certs, mqtt_bridge_hostname,
-        mqtt_bridge_port)
-
-    attach_device(client, device_id, '')
-    print('Waiting for device to attach.')
-    time.sleep(5)
-
-    # The topic devices receive configuration updates on.
-    device_config_topic = '/devices/{}/config'.format(device_id)
-    client.subscribe(device_config_topic, qos=1)
-
-    # The topic gateways receive configuration updates on.
-    gateway_config_topic = '/devices/{}/config'.format(gateway_id)
-    client.subscribe(gateway_config_topic, qos=1)
-
-    # The topic gateways receive error updates on. QoS must be 0.
-    error_topic = '/devices/{}/errors'.format(gateway_id)
-    client.subscribe(error_topic, qos=0)
-
-    # Wait for about a minute for config messages.
-    for i in range(1, duration):
-        client.loop()
-        if cb is not None:
-            cb(client)
-
-        if should_backoff:
-            # If backoff time is too large, give up.
-            if minimum_backoff_time > MAXIMUM_BACKOFF_TIME:
-                print('Exceeded maximum backoff time. Giving up.')
-                break
-
-            delay = minimum_backoff_time + random.randint(0, 1000) / 1000.0
-            time.sleep(delay)
-            minimum_backoff_time *= 2
-            client.connect(mqtt_bridge_hostname, mqtt_bridge_port)
-
-        seconds_since_issue = (datetime.datetime.utcnow() - jwt_iat).seconds
-        if seconds_since_issue > 60 * jwt_exp_mins:
-            print('Refreshing token after {}s').format(seconds_since_issue)
-            jwt_iat = datetime.datetime.utcnow()
-            client = get_client(
-                project_id, cloud_region, registry_id, gateway_id,
-                private_key_file, algorithm, ca_certs, mqtt_bridge_hostname,
-                mqtt_bridge_port)
-
-        time.sleep(1)
-
-    detach_device(client, device_id)
-
-    print('Finished.')
-    # [END listen_for_messages]
+    pass
 
 
 def send_data_from_bound_device(
         service_account_json, project_id, cloud_region, registry_id, device_id,
         gateway_id, num_messages, private_key_file, algorithm, ca_certs,
         mqtt_bridge_hostname, mqtt_bridge_port, jwt_expires_minutes, payload):
-    """Sends data from a gateway on behalf of a device that is bound to it."""
-    # [START send_data_from_bound_device]
-    global minimum_backoff_time
-
-    # Publish device events and gateway state.
-    device_topic = '/devices/{}/{}'.format(device_id, 'state')
-    gateway_topic = '/devices/{}/{}'.format(gateway_id, 'state')
-
-    jwt_iat = datetime.datetime.utcnow()
-    jwt_exp_mins = jwt_expires_minutes
-    # Use gateway to connect to server
-    client = get_client(
-        project_id, cloud_region, registry_id, gateway_id,
-        private_key_file, algorithm, ca_certs, mqtt_bridge_hostname,
-        mqtt_bridge_port)
-
-    attach_device(client, device_id, '')
-    print('Waiting for device to attach.')
-    time.sleep(5)
-
-    # Publish state to gateway topic
-    gateway_state = 'Starting gateway at: {}'.format(time.time())
-    print(gateway_state)
-    client.publish(gateway_topic, gateway_state, qos=1)
-
-    # Publish num_messages mesages to the MQTT bridge
-    # for i in range(1, num_messages + 1):
-
-    parser = argparse.ArgumentParser()
-    model = './edgetpu/test_data/mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite'
-    parser.add_argument('--model', help='Path of the detection model.', default=model)
-    parser.add_argument('--label', help='Path of the labels file.')
-    parser.add_argument('--input', help='File path of the input image.', required=False)
-    parser.add_argument('--output', help='File path of the output image.')
-    args = parser.parse_args()
-
-    # Initialize engine.
-    engine = DetectionEngine(args.model)
-    labels = ReadLabelFile(args.label) if args.label else None
-
-    with picamera.PiCamera() as camera:
-        camera.resolution = (1028, 712)
-        camera.framerate = 30
-        _, width, height, channels = engine.get_input_tensor_shape()
-        camera.start_preview()
-        try:
-            stream = io.BytesIO()
-            for foo in camera.capture_continuous(stream,
-                                                 format='rgb',
-                                                 use_video_port=True,
-                                                 resize=(width, height)):
-                client.loop()
-
-                if should_backoff:
-                    # If backoff time is too large, give up.
-                    if minimum_backoff_time > MAXIMUM_BACKOFF_TIME:
-                        print('Exceeded maximum backoff time. Giving up.')
-                        break
-
-                    delay = minimum_backoff_time + random.randint(0, 1000) / 1000.0
-                    time.sleep(delay)
-                    minimum_backoff_time *= 2
-                    client.connect(mqtt_bridge_hostname, mqtt_bridge_port)
-
-                stream.truncate()
-                stream.seek(0)
-                input = np.frombuffer(stream.getvalue(), dtype=np.uint8)
-                # cv2.imwrite('current_frame.jpg', input)
-                # img = Image.open('current_frame.jpg')
-                # draw = ImageDraw.Draw(img)
-                start_ms = time.time()
-                ans = engine.DetectWithInputTensor(input, threshold=0.5, top_k=10)
-                elapsed_ms = time.time() - start_ms
-                # Display result.
-                print ('-----------------------------------------')
-                nPerson = 0
-                bbox = list()
-                scores = list()
-                if ans:
-                    print(ans)
-                    for obj in ans:
-                        nPerson = nPerson + 1
-                        if labels:
-                            print(labels[obj.label_id])
-                        score = obj.score
-                        print ('score = ', obj.score)
-                        box = obj.bounding_box.flatten().tolist()
-                        print ('box = ', box)
-                        bbox.append(box)
-                        scores.append(score)
-                    # msg = {"nPersons":float(len(ans)), "bounding_box":str(bbox), "scores": str(scores)}
-                    print("nPerson = " + str(nPerson))
-                    msg = {"nPersons": int(nPerson)}
-                    # print(msg)
-                    # return msg
-                else:
-                    print ('No object detected!')
-                    # msg = {"nPersons":float(0), "bounding_box":str(bbox), "scores": str(scores)}
-                    msg = {"nPersons": int(nPerson)}
-                    # print(msg)
-                    # return msg
-
-                payload = '{}/{}-{}-payload-{}'.format(
-                    nPerson, nPerson, nPerson, i)
-
-                print('Publishing message {}/{}: \'{}\' to {}'.format(
-                    i, num_messages, payload, device_topic))
-                client.publish(
-                    device_topic, '{} : {}'.format(device_id, payload), qos=1)
-
-                seconds_since_issue = (datetime.datetime.utcnow() - jwt_iat).seconds
-                if seconds_since_issue > 60 * jwt_exp_mins:
-                    print('Refreshing token after {}s').format(seconds_since_issue)
-                    jwt_iat = datetime.datetime.utcnow()
-                    client = get_client(
-                        project_id, cloud_region, registry_id, gateway_id,
-                        private_key_file, algorithm, ca_certs, mqtt_bridge_hostname,
-                        mqtt_bridge_port)
-
-                time.sleep(5)
-
-            detach_device(client, device_id)
-
-            print('Finished.')
-
-        finally:
-          camera.stop_preview()
-    # [END send_data_from_bound_device]
-
+    pass
 
 def parse_command_line_args():
     """Parse command line arguments."""
@@ -511,18 +324,6 @@ def mqtt_device_demo(args):
         args.device_id, args.private_key_file, args.algorithm,
         args.ca_certs, args.mqtt_bridge_hostname, args.mqtt_bridge_port)
 
-
-
-
-
-    # parser_tpu = argparse.ArgumentParser()
-    # model = './edgetpu/test_data/mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite'
-    # parser_tpu.add_argument('--model', help='Path of the detection model.', default=model)
-    # # parser.add_argument('--label', help='Path of the labels file.')
-    # # parser.add_argument('--input', help='File path of the input image.', required=False)
-    # # parser.add_argument('--output', help='File path of the output image.')
-    # args_tpu = parser_tpu.parse_args()
-
     # Initialize engine.
     engine = DetectionEngine('./edgetpu/test_data/mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite')
     # labels = ReadLabelFile(args_tpu.label) if args_tpu.label else None
@@ -550,6 +351,8 @@ def mqtt_device_demo(args):
                     time.sleep(delay)
                     minimum_backoff_time *= 2
                     client.connect(mqtt_bridge_hostname, mqtt_bridge_port)
+
+                now = datetime.datetime.now()
 
                 stream.truncate()
                 stream.seek(0)
@@ -586,7 +389,7 @@ def mqtt_device_demo(args):
                 scores_msg = [{'score': s[0]} for s in eval(msg["scores"])]
 
                 info = {'nPersons': '{}'.format(nPerson), 'bounding_box': bounding_box,
-                        'scores': scores_msg}
+                        'scores': scores_msg, 'TimeStamp': str(now)}
 
                 info = json.dumps(info)
 
