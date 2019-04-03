f8e3cb22ee8f373e11ef0af5d8edf13eda8e4d31
ocimk <kafashanm@objectcomputing.com>
2019-04-03 09:21:57 -0500

    clean iot core scripts

--- cloud-iot-direct/mqtt/env/lib/python3.6/base64.py
+++
@@ -1,1 +0,0 @@
-/home/ms/anaconda3_2/lib/python3.6/base64.py
--- cloud-iot-direct/security_cam_cloudiot.py
+++ cloud-iot-direct/security_cam_cloudiot.py
@@ -13,12 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Python sample for connecting to Google Cloud IoT Core via MQTT, using JWT.
-This example connects to Google Cloud IoT Core via MQTT, using a JWT for device
-authentication. After connecting, by default the device publishes 100 messages
-to the device's MQTT topic at a rate of one per second, and then exits.
-Before you run the sample, you must follow the instructions in the README
-for this sample.
+"""
+Cloud IOT Core‎ using raspberry pie as the device to send number of faces, timestamps, scores and bounding box information. 
 """
 
 # [START iot_mqtt_includes]
@@ -55,25 +51,12 @@ minimum_backoff_time = 1
 # The maximum backoff time before giving up, in seconds.
 MAXIMUM_BACKOFF_TIME = 32
 
-# Whether to wait with exponential backoff before publishing.
+# Whether to wait with exponential back-off before publishing.
 should_backoff = False
 
 
 # [START iot_mqtt_jwt]
 def create_jwt(project_id, private_key_file, algorithm):
-    """Creates a JWT (https://jwt.io) to establish an MQTT connection.
-        Args:
-         project_id: The cloud project ID this device belongs to
-         private_key_file: A path to a file containing either an RSA256 or
-                 ES256 private key.
-         algorithm: The encryption algorithm to use. Either 'RS256' or 'ES256'
-        Returns:
-            An MQTT generated from the given project_id and private key, which
-            expires in 20 minutes. After 20 minutes, your client will be
-            disconnected, and a new JWT will have to be generated.
-        Raises:
-            ValueError: If the private_key_file does not contain a known key.
-        """
 
     token = {
         # The time that the token was issued at
@@ -394,21 +377,18 @@ def mqtt_device_demo(args):
                 info = json.dumps(info)
 
                 payload = info
-                print('Publishing message {}/{}: \'{}\''.format(nPerson, args.num_messages, payload))
+                print('Publishing message {}'.format(payload))
                 # [START iot_mqtt_jwt_refresh]
                 seconds_since_issue = (datetime.datetime.utcnow() - jwt_iat).seconds
                 if seconds_since_issue > 60 * jwt_exp_mins:
-                    print('Refreshing token after {}s').format(seconds_since_issue)
+                    # print('Refreshing token')
                     jwt_iat = datetime.datetime.utcnow()
                     client = get_client(
                         args.project_id, args.cloud_region,
                         args.registry_id, args.device_id, args.private_key_file,
                         args.algorithm, args.ca_certs, args.mqtt_bridge_hostname,
                         args.mqtt_bridge_port)
-        # [END iot_mqtt_jwt_refresh]
-        # Publish "payload" to the MQTT topic. qos=1 means at least once
-        # delivery. Cloud IoT Core also supports qos=0 for at most once
-        # delivery.
+
                 client.publish(mqtt_topic, payload, qos=1)
 
         # Send events every second. State should not be updated as often
