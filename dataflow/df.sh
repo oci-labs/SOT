d91447bd201147c86c6d7425a6faeeb23f422606
Mehdi Khodayari <khodayarim@objectcomputing.com>
2019-04-04 10:11:00 -0500

    Remove df folder

--- dataflow/df.sh
+++
@@ -1,5 +0,0 @@
-python apache_beam_pub_sub_to_bq.py \
-    --input_topic=projects/securityofthings/topics/mk_topic_6 \
-    --output_path=gs://pub_sub_temp/temp/ \
-    --input_subscription=projects/securityofthings/subscriptions/sot-subscription \
-    --experiments=allow_non_updatable_job
--- dataflow/pubsub.sh
+++
@@ -1,1 +0,0 @@
-python pub_sub.py
